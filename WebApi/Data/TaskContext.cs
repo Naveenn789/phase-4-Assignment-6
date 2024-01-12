fca40372832401b6e146776f860fd78adf8507c2
Naveenn789 <naveengalla77@gmail.com>
2024-01-12 16:34:43 +0530

    'Docker'

---
+++ WebApi/Data/TaskContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext (DbContextOptions<TaskContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApi.Models.Task> Task { get; set; } = default!;
+    }
+}
---
+++ WebApi/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models
+{
+    [Table("Task")]
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+    }
+}
